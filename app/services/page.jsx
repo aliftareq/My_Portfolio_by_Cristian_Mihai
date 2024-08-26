b53d2015630fbce9587cf6fa4dc9d29f76e3b80b
alif <aliftareq@gmail.com>
2024-08-26 15:38:41 +0600

    Services page completed

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,7 +1,81 @@
-import React from "react";
+"use client";
+
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowDownRight } from "react-icons/bs";
+
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Experienced web developer specializing in creating responsive, user-friendly websites and scalable web applications. Passionate about delivering clean code and innovative solutions tailored to your business needs.",
+  },
+  {
+    num: "02",
+    title: "UI/UX Design",
+    description:
+      "Creative UI/UX designer focused on crafting intuitive and visually appealing digital experiences. I transform user insights into seamless, engaging designs that drive interaction and satisfaction.",
+  },
+  {
+    num: "03",
+    title: "App Development",
+    description:
+      "Skilled app developer delivering high-performance mobile and web apps tailored to your needs. I turn ideas into fully functional, user-centric applications with smooth interfaces and robust backend solutions.",
+  },
+  {
+    num: "04",
+    title: "SEO",
+    description:
+      "SEO specialist optimizing websites for higher search engine rankings, increased traffic, and better online visibility. I use data-driven strategies to boost organic growth and enhance your digital presence.",
+  },
+];
 
 const Services = () => {
-  return <div>Services page</div>;
+  return (
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
+          }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+        >
+          {services.map((service, index) => {
+            return (
+              <div
+                key={index}
+                className="flex-1 flex flex-col justify-center gap-6 group"
+              >
+                {/* top  */}
+                <div className="w-full flex justify-between items-center">
+                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+                    {service.num}
+                  </div>
+                  <Link
+                    href="/"
+                    className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                  >
+                    <BsArrowDownRight className="text-primary text-3xl" />
+                  </Link>
+                </div>
+                {/* title */}
+                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
+                  {service.title}
+                </h2>
+                {/* description  */}
+                <p className="text-white/60">{service.description}</p>
+                {/* border */}
+                <div className="border-b border-white/20 w-full"></div>
+              </div>
+            );
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
